afe10571c6ecd66389e205c7f805ee7303186558
Matthew Field <matthew.field@ukaea.uk>
2022-11-03 15:25:31 +0000

    Improved models docstrings

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,16 +19,25 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+    :param data: A 2D array containing the inflammation data for all patients.
+    :returns: A 1D array of the mean value of inflammation for each day.
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    :param data: A 2D array containing the inflammation data for all patients.
+    :returns: A 1D array of the mean value of inflammation for each day.
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    :param data: A 2D array containing the inflammation data for all patients.
+    :returns: A 1D array of the mean value of inflammation for each day.
+    """
     return np.min(data, axis=0)
 
